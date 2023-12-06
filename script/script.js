6904ee33af479f001fb11f5dd7171b37f68fa272
TigranPoghos <tigran2002poghosyan@gmail.com>
2023-12-06 23:44:22 +0300

    catalog

--- script/script.js
+++ script/script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    const body = document.querySelector('body')
+
     //каталог
     const header__catalogJs = document.querySelector('.header__catalogJs')
-    const header__catalog = document.querySelector('.header__catalog')
+    const header__catalog = document.querySelector('.header__catalog') 
+    const header = document.querySelector('.header')
+    const header__opacity = document.querySelector('.header__opacity')
 
     header__catalogJs.addEventListener('click', function(){
 	    header__catalog.classList.toggle('active')
+        header.classList.toggle('active')
+        body.classList.toggle('active')
+        header__opacity.classList.toggle('active')
     })
 
 
@@ -16,6 +23,9 @@ document.addEventListener("DOMContentLoaded", function(){
     
         if (( !click ) && (!clickJs)) {
             header__catalog.classList.remove('active')
+            header.classList.remove('active')
+            header__opacity.classList.remove('active')
+            body.classList.remove('active')
         }
     })
 

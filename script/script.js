20e243ac7d0ba9ff71540239b3870da6ac499166
TigranPoghos <tigran2002poghosyan@gmail.com>
2023-12-06 16:58:54 +0300

    header click

--- script/script.js
+++ script/script.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+    //каталог
+    const header__catalogJs = document.querySelector('.header__catalogJs')
+    const header__catalog = document.querySelector('.header__catalog')
+
+    header__catalogJs.addEventListener('click', function(){
+	    header__catalog.classList.toggle('active')
+    })
+
+
+    //клик вне каталога
+    document.addEventListener('click', (e) => {
+        const click = e.composedPath().includes(header__catalog)
+        const clickJs = e.composedPath().includes(header__catalogJs)
+    
+        if (( !click ) && (!clickJs)) {
+            header__catalog.classList.remove('active')
+        }
+    })
+
+
 })

be5129d468fb86463859c545ae36808ea7346719
TigranPoghos <tigran2002poghosyan@gmail.com>
2023-12-18 17:08:20 +0300

    login

--- script/product.js
+++ script/product.js
@@ -31,6 +31,73 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
+    //корзина
+    const basket__button = document.querySelector('.basket__button')
+    const basket = document.querySelector('.basket')
+    const basket__opacity = document.querySelector('.basket__opacity')
+
+    basket__button.addEventListener('click', function(){
+        basket.classList.toggle('active')
+        body.classList.toggle('activeBasket')
+        header.classList.toggle('activeBasket')
+        basket__opacity.classList.toggle('active')
+    })
+
+    const basket__close = document.querySelector('.basket__close')
+
+    basket__close.addEventListener('click', function(){
+        basket.classList.toggle('active')
+        body.classList.toggle('activeBasket')
+        header.classList.toggle('activeBasket')
+        basket__opacity.classList.toggle('active')
+    })
+
+    //клик вне корзины
+    document.addEventListener('click', (e) => {
+        const click = e.composedPath().includes(basket)
+        const clickBasket = e.composedPath().includes(basket__button)
+    
+        if (( !click ) && (!clickBasket)) {
+            basket.classList.remove('active')
+            body.classList.remove('activeBasket')
+            header.classList.remove('activeBasket')
+            basket__opacity.classList.remove('active')
+        }
+    })
+
+
+
+
+        // Убавляем кол-во по клику
+    $('.quantity_inner .bt_minus').click(function() {
+        let $input = $(this).parent().find('.quantity')
+        let count = parseInt($input.val()) - 1
+        count = count < 1 ? 1 : count
+        $input.val(count)
+    });
+    // Прибавляем кол-во по клику
+    $('.quantity_inner .bt_plus').click(function() {
+        let $input = $(this).parent().find('.quantity')
+        let count = parseInt($input.val()) + 1
+        count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count
+        $input.val(parseInt(count))
+    }); 
+    // Убираем все лишнее и невозможное при изменении поля
+    $('.quantity_inner .quantity').bind("change keyup input click", function() {
+        if (this.value.match(/[^0-9]/g)) {
+            this.value = this.value.replace(/[^0-9]/g, '')
+        }
+        if (this.value == "") {
+            this.value = 1
+        }
+        if (this.value > parseInt($(this).data('max-count'))) {
+            this.value = parseInt($(this).data('max-count'))
+        }    
+    }); 
+    
+
+
+
 
         //карточка товара
         const product__infoOpen = document.querySelector('.product__info-open')
